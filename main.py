5fa1b07a32109d08e48e181199636746b33579c0
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2024-09-27 16:42:05 -0300

    pltshow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
 import mysql.connector
 import face_recognition
+import os
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 db_config = {
     'user': 'root',
@@ -10,21 +11,17 @@ db_config = {
     'database': 'tcc'
 }
 
+db_conn = mysql.connector.connect(**db_config)
+cursor = db_conn.cursor()
 
-def connect_to_database(config):
-    try:
-        return mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-        print(f"Erro ao conectar ao banco de dados: {err}")
-        return None
+def get_solicitantes_images(directory):
+    images = []
+    for file in os.listdir(directory):
+        if file.endswith(('jpg', 'jpeg', 'png')):
+            images.append(os.path.join(directory, file))
+    return images
 
-
-def get_images_from_directory(directory):
-    return [os.path.join(directory, file) for file in os.listdir(directory)
-            if file.endswith(('jpg', 'jpeg', 'png'))]
-
-
-def get_blacklist_images(cursor):
+def get_blacklist_images():
     query = """
         SELECT blacklist.pesid, imagem.caminho_imagem 
         FROM blacklist
@@ -33,78 +30,71 @@ def get_blacklist_images(cursor):
     cursor.execute(query)
     return [(pesid, os.path.basename(caminho_imagem)) for pesid, caminho_imagem in cursor.fetchall()]
 
+def compare_images(solicitante_image, blacklist_images, base_dir):
+    solicitante_img = face_recognition.load_image_file(solicitante_image)
+    solicitante_encoding = face_recognition.face_encodings(solicitante_img)
 
-def compare_images(solicitante_image, blacklist_images, blacklist_dir):
-    try:
-        solicitante_img = face_recognition.load_image_file(solicitante_image)
-        solicitante_encoding = face_recognition.face_encodings(solicitante_img)
+    if not solicitante_encoding:
+        return None
 
-        if not solicitante_encoding:
-            print(f"Falha ao codificar a imagem: {solicitante_image}")
-            return None
+    for pesid, fraud_image_name in blacklist_images:
+        fraud_image_path = os.path.join(base_dir, fraud_image_name)
+        if os.path.isfile(fraud_image_path):
+            fraud_img = face_recognition.load_image_file(fraud_image_path)
+            fraud_encoding = face_recognition.face_encodings(fraud_img)
 
-        for pesid, blacklist_image in blacklist_images:
-            blacklist_image_path = os.path.join(blacklist_dir, blacklist_image)
-            if os.path.isfile(blacklist_image_path):
-                fraud_img = face_recognition.load_image_file(blacklist_image_path)
-                fraud_encoding = face_recognition.face_encodings(fraud_img)
+            if not fraud_encoding:
+                continue
 
-                if fraud_encoding and face_recognition.compare_faces([fraud_encoding[0]], solicitante_encoding[0])[0]:
-                    return pesid
-
-    except Exception as e:
-        print(f"Erro ao comparar imagens: {e}")
+            match = face_recognition.compare_faces([fraud_encoding[0]], solicitante_encoding[0])
+            if match[0]:
+                return pesid
     return None
 
-
-def insert_suspeita(cursor, pesid):
-    """Insere um registro de suspeita no banco de dados."""
-    try:
-        now = datetime.now()
-        query = "INSERT INTO solicitacao_suspeita (pesid, data) VALUES (%s, %s)"
-        cursor.execute(query, (pesid, now))
-        print(f"Suspeita registrada para pesid: {pesid}")
-    except mysql.connector.Error as err:
-        print(f"Erro ao inserir suspeita: {err}")
-
-
-def process_images(db_conn, solicitantes_images, blacklist_images, blacklist_dir):
-    cursor = db_conn.cursor()
-
-    for solicitante_image in solicitantes_images:
-        print(f"Processando imagem: {solicitante_image}")
-
-        pesid = compare_images(solicitante_image, blacklist_images, blacklist_dir)
-
-        if pesid:
-            insert_suspeita(cursor, pesid)
-
+def insert_suspeita(pesid):
+    now = datetime.now()
+    query = "INSERT INTO solicitacao_suspeita (pesid, data) VALUES (%s, %s)"
+    cursor.execute(query, (pesid, now))
     db_conn.commit()
-    cursor.close()
 
+def gerar_grafico(resultados_suspeitas):
+    labels = list(resultados_suspeitas.keys())
+    values = list(resultados_suspeitas.values())
 
-def main():
-    db_conn = connect_to_database(db_config)
-    if not db_conn:
-        return
+    plt.bar(labels, values, color=['blue', 'orange'])
+    plt.title('Número de Correspondências Suspeitas por Diretório')
+    plt.xlabel('Diretórios')
+    plt.ylabel('Número de Correspondências')
 
-    solicitantes_dir = r'C:\Users\joao.mendonca\Desktop\face_recognition\Solicitantes'
-    blacklist_dir = r'C:\Users\joao.mendonca\Desktop\face_recognition\blacklist'
+    plt.show()
 
-    try:
-        solicitantes_images = get_images_from_directory(solicitantes_dir)
+solicitantes_dir = r'C:\Users\joao.mendonca\Desktop\face_recognition\Solicitantes'
+homem_dir = r'C:\Users\joao.mendonca\Desktop\face_recognition\Homem'
+mulher_dir = r'C:\Users\joao.mendonca\Desktop\face_recognition\Mulher'
 
-        cursor = db_conn.cursor()
-        blacklist_images = get_blacklist_images(cursor)
+solicitantes_images = get_solicitantes_images(solicitantes_dir)
 
-        if not solicitantes_images:
-            print("Nenhuma imagem de solicitante encontrada.")
-        else:
-            process_images(db_conn, solicitantes_images, blacklist_images, blacklist_dir)
+blacklist_images = get_blacklist_images()
 
-    finally:
-        db_conn.close()
+resultados_suspeitas = {
+    'Homem': 0,
+    'Mulher': 0
+}
 
+for solicitante_image in solicitantes_images:
+    fraudador_pesid = compare_images(solicitante_image, blacklist_images, homem_dir)
+    if fraudador_pesid:
+        insert_suspeita(fraudador_pesid)
+        resultados_suspeitas['Homem'] += 1
+        print(f"Fraudador identificado no diretório 'Homem': {fraudador_pesid}")
 
-if __name__ == "__main__":
-    main()
+    fraudador_pesid = compare_images(solicitante_image, blacklist_images, mulher_dir)
+    if fraudador_pesid:
+        insert_suspeita(fraudador_pesid)
+        resultados_suspeitas['Mulher'] += 1
+        print(f"Fraudador identificado no diretório 'Mulher': {fraudador_pesid}")
+
+gerar_grafico(resultados_suspeitas)
+
+cursor.close()
+db_conn.close()
--- zip_folders.py
+++ zip_folders.py
@@ -2,9 +2,19 @@ import os
 import zipfile
 import shutil
 
+def remove_existing_zip(zip_file_name):
+    """Remove o arquivo ZIP se ele já existir."""
+    if os.path.exists(zip_file_name):
+        os.remove(zip_file_name)
+        print(f"Arquivo {zip_file_name} removido.")
+    else:
+        print(f"Arquivo {zip_file_name} não encontrado.")
+
 
 def zip_folder(folder_name):
     zip_file_name = f"{folder_name}.zip"
+
+    remove_existing_zip(zip_file_name)
 
     with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for root, dirs, files in os.walk(folder_name):
@@ -16,6 +26,7 @@ def zip_folder(folder_name):
 
 
 def remove_folder(folder_name):
+    """Exclui a pasta após compactação."""
     if os.path.exists(folder_name):
         shutil.rmtree(folder_name)
         print(f"Pasta {folder_name} foi excluída.")
